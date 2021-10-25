57862277ad946835f1bf91af0139eea6fa86b253
Omar Alshayeb <omarblitz9@gmail.com>
2021-10-25 07:59:06 -0700

    prep1

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,28 +1,28 @@
-const multer = require('multer');
-const sharp = require('sharp');
-const Tour = require('./../models/tourModel');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
-const factory = require('./handlerFactory');
+const multer = require("multer");
+const sharp = require("sharp");
+const Tour = require("./../models/tourModel");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+const factory = require("./handlerFactory");
 
 const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image')) {
+  if (file.mimetype.startsWith("image")) {
     cb(null, true);
   } else {
-    cb(new AppError('Not an image! Please upload only images.', 400), false);
+    cb(new AppError("Not an image! Please upload only images.", 400), false);
   }
 };
 
 const upload = multer({
   storage: multerStorage,
-  fileFilter: multerFilter
+  fileFilter: multerFilter,
 });
 
 exports.uploadTourImages = upload.fields([
-  { name: 'imageCover', maxCount: 1 },
-  { name: 'images', maxCount: 3 }
+  { name: "imageCover", maxCount: 1 },
+  { name: "images", maxCount: 3 },
 ]);
 
 exports.resizeTourImages = catchAsync(async (req, res, next) => {
@@ -32,7 +32,7 @@ exports.resizeTourImages = catchAsync(async (req, res, next) => {
   req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
   await sharp(req.files.imageCover[0].buffer)
     .resize(2000, 1333)
-    .toFormat('jpeg')
+    .toFormat("jpeg")
     .jpeg({ quality: 90 })
     .toFile(`public/img/tours/${req.body.imageCover}`);
 
@@ -45,7 +45,7 @@ exports.resizeTourImages = catchAsync(async (req, res, next) => {
 
       await sharp(file.buffer)
         .resize(2000, 1333)
-        .toFormat('jpeg')
+        .toFormat("jpeg")
         .jpeg({ quality: 90 })
         .toFile(`public/img/tours/${filename}`);
 
@@ -53,51 +53,51 @@ exports.resizeTourImages = catchAsync(async (req, res, next) => {
     })
   );
 
-  console.log(req.body);
+  // console.log(req.body);
   next();
 });
 
 exports.getAllTours = factory.getAll(Tour);
-exports.getTour = factory.getOne(Tour, { path: 'reviews' });
+exports.getTour = factory.getOne(Tour, { path: "reviews" });
 exports.createTour = factory.createOne(Tour);
 exports.updateTour = factory.updateOne(Tour);
 exports.deleteTour = factory.deleteOne(Tour);
 
 exports.aliasTopTour = (req, res, next) => {
-  req.query.limit = '5';
-  req.query.sort = '-ratingsaverage,price';
-  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  req.query.limit = "5";
+  req.query.sort = "-ratingsaverage,price";
+  req.query.fields = "name,price,ratingsAverage,summary,difficulty";
   next();
 };
 
 exports.getTourStats = catchAsync(async (req, res, next) => {
   const stats = await Tour.aggregate([
     {
-      $match: { ratingsAverage: { $gte: 4.5 } }
+      $match: { ratingsAverage: { $gte: 4.5 } },
     },
     {
       $group: {
-        _id: { $toUpper: '$difficulty' },
+        _id: { $toUpper: "$difficulty" },
         numTours: { $sum: 1 },
-        numRatings: { $sum: '$ratingsQuantity' },
-        avgRating: { $avg: '$ratingsAverage' },
-        avgPrice: { $avg: '$price' },
-        minPrice: { $min: '$price' },
-        maxPrice: { $max: '$price' }
-      }
+        numRatings: { $sum: "$ratingsQuantity" },
+        avgRating: { $avg: "$ratingsAverage" },
+        avgPrice: { $avg: "$price" },
+        minPrice: { $min: "$price" },
+        maxPrice: { $max: "$price" },
+      },
     },
     {
-      $sort: { avgPrice: 1 }
-    }
+      $sort: { avgPrice: 1 },
+    },
     // {
     //   $match: { _id: { $ne: 'EASY' } }
     // }
   ]);
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: {
-      stats
-    }
+      stats,
+    },
   });
 });
 
@@ -106,85 +106,85 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
 
   const plan = await Tour.aggregate([
     {
-      $unwind: '$startDates'
+      $unwind: "$startDates",
     },
     {
       $match: {
         startDates: {
           $gte: new Date(`${year}-01-01`),
-          $lte: new Date(`${year}-12-31`)
-        }
-      }
+          $lte: new Date(`${year}-12-31`),
+        },
+      },
     },
     {
       $group: {
-        _id: { $month: '$startDates' },
+        _id: { $month: "$startDates" },
         numTourStarts: { $sum: 1 },
-        tours: { $push: '$name' }
-      }
+        tours: { $push: "$name" },
+      },
     },
     {
-      $addFields: { month: '$_id' }
+      $addFields: { month: "$_id" },
     },
     {
       $project: {
-        _id: 0
-      }
+        _id: 0,
+      },
     },
     {
-      $sort: { numTourStarts: -1 }
+      $sort: { numTourStarts: -1 },
     },
     {
-      $limit: 12
-    }
+      $limit: 12,
+    },
   ]);
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: {
-      plan
-    }
+      plan,
+    },
   });
 });
 
 // /tours-within/233/center/-40,45/unit/mi
 exports.getTourWithin = catchAsync(async (req, res, next) => {
   const { distance, latlng, unit } = req.params;
-  const [lat, lng] = latlng.split(',');
+  const [lat, lng] = latlng.split(",");
 
-  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1; //miles or kilometers
+  const radius = unit === "mi" ? distance / 3963.2 : distance / 6378.1; //miles or kilometers
 
   if (!lat || !lng) {
     next(
       new AppError(
-        'Please provide latitude and longitude in the format lat,lng.',
+        "Please provide latitude and longitude in the format lat,lng.",
         400
       )
     );
   }
 
   const tours = await Tour.find({
-    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
   });
 
   res.status(200).json({
-    status: 'success',
+    status: "success",
     results: tours.length,
     data: {
-      data: tours
-    }
+      data: tours,
+    },
   });
 });
 
 exports.getDistances = catchAsync(async (req, res, next) => {
   const { latlng, unit } = req.params;
-  const [lat, lng] = latlng.split(',');
+  const [lat, lng] = latlng.split(",");
 
-  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+  const multiplier = unit === "mi" ? 0.000621371 : 0.001;
 
   if (!lat || !lng) {
     next(
       new AppError(
-        'Please provide latitude and longitude in the format lat,lng.',
+        "Please provide latitude and longitude in the format lat,lng.",
         400
       )
     );
@@ -194,25 +194,25 @@ exports.getDistances = catchAsync(async (req, res, next) => {
     {
       $geoNear: {
         near: {
-          type: 'Point',
-          coordinates: [lng * 1, lat * 1]
+          type: "Point",
+          coordinates: [lng * 1, lat * 1],
         },
-        distanceField: 'distance',
-        distanceMultiplier: multiplier
-      }
+        distanceField: "distance",
+        distanceMultiplier: multiplier,
+      },
     },
     {
       $project: {
         distance: 1,
-        name: 1
-      }
-    }
+        name: 1,
+      },
+    },
   ]);
 
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: {
-      data: distances
-    }
+      data: distances,
+    },
   });
 });
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,24 +1,24 @@
 /*eslint-disable*/
-import axios from 'axios';
-import { showAlert } from './alert';
+import axios from "axios";
+import { showAlert } from "./alert";
 
 //type is either 'password' or data
 export const updateSettings = async (data, type) => {
   try {
     const url =
-      type === 'password'
-        ? 'http://127.0.0.1:8000/api/v1/users/updateMyPassword'
-        : 'http://127.0.0.1:8000/api/v1/users/updateMe';
+      type === "password"
+        ? "/api/v1/users/updateMyPassword"
+        : "/api/v1/users/updateMe";
     const res = await axios({
-      method: 'PATCH',
+      method: "PATCH",
       url,
-      data
+      data,
     });
 
-    if (res.data.status === 'success') {
-      showAlert('success', `${type.toUpperCase()} Updated successfully!`);
+    if (res.data.status === "success") {
+      showAlert("success", `${type.toUpperCase()} Updated successfully!`);
     }
   } catch (err) {
-    showAlert('error', err.response.data.message);
+    showAlert("error", err.response.data.message);
   }
 };
